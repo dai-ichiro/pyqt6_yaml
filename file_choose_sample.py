4855859fd95871b8beb8e9304e513a9a15752367
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-24 08:15:30 +0900

    update script

--- file_choose_sample.py
+++ file_choose_sample.py
@@ -8,9 +8,11 @@ fname = os.path.basename(url)
 if not os.path.isfile(fname):
     urlretrieve(url, fname)
 '''
+
 from PyQt6 import QtCore
+from PyQt6.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout, QFileDialog
 from PyQt6.QtGui import QImage, QPixmap
-from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QVBoxLayout, QFileDialog
+
 from constructGUI import construct
 
 class Window(QWidget):
@@ -18,20 +20,20 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        
+
     def initUI(self):
         self.setWindowTitle("pyQt6 sample")
-
-        self.img_label = construct(QLabel(), 'settings.yaml', 'label_1')
+        
         self.button1 = construct(QPushButton(), 'settings.yaml', 'button_1')
-        
-        layout = QVBoxLayout()
-        layout.addWidget(self.img_label)
-        layout.addWidget(self.button1)
-        
-        self.setLayout(layout)
+        self.img_label1 = construct(QLabel(), 'settings.yaml', 'label_1')    
 
         self.button1.clicked.connect(self.showDialog1)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.img_label1)
+        layout.addWidget(self.button1)
+
+        self.setLayout(layout)
     
     def showDialog1(self):
 
@@ -40,9 +42,9 @@ class Window(QWidget):
         # fname[0]は選択したファイルのパス（ファイル名を含む）
         if fname[0]:
             # 画像の読み込み, サイズ変更
-            image = QImage(fname[0]).scaled(512,512,QtCore.Qt.AspectRatioMode.KeepAspectRatio) 
+            image = QImage(fname[0]).scaled(self.img_label1.size(),QtCore.Qt.AspectRatioMode.KeepAspectRatio) 
             # 画像の表示
-            self.img_label.setPixmap(QPixmap.fromImage(image))
+            self.img_label1.setPixmap(QPixmap.fromImage(image))
 
 if __name__ == "__main__":
     app = QApplication([])
