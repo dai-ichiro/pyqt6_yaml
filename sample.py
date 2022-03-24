182e7533917c9756f355767db09fc1cf84bd34d8
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-24 23:18:07 +0900

    update

--- sample.py
+++ sample.py
@@ -9,8 +9,7 @@ if not os.path.isfile(fname):
     urlretrieve(url, fname)
 '''
 
-from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtWidgets import QWidget, QApplication, QSlider, QVBoxLayout, QFrame
+from PyQt6.QtWidgets import QWidget, QApplication, QSlider, QVBoxLayout
 from constructGUI import construct
 
 class Window(QWidget):
@@ -23,9 +22,19 @@ class Window(QWidget):
         self.setWindowTitle("sample")
 
         self.slider_1 = construct(QSlider(), 'settings.yaml', 'slider_1')
+        self.slider_2 = construct(QSlider(), 'settings.yaml', 'slider_1')
+        self.slider_3 = construct(QSlider(), 'settings.yaml', 'slider_1')
+        self.slider_4 = construct(QSlider(), 'settings.yaml', 'slider_1')
 
+        self.slider_2.setTickPosition(QSlider.TickPosition.TicksLeft)
+        self.slider_3.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.slider_4.setTickPosition(QSlider.TickPosition.TicksBothSides)
+        
         layout = QVBoxLayout()
         layout.addWidget(self.slider_1)
+        layout.addWidget(self.slider_2)
+        layout.addWidget(self.slider_3)
+        layout.addWidget(self.slider_4)
 
         self.setLayout(layout)
 
