fea019872bd082457350708f04158e174cc84c1a
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-24 22:31:13 +0900

    update

--- sample.py
+++ sample.py
@@ -8,8 +8,8 @@ fname = os.path.basename(url)
 if not os.path.isfile(fname):
     urlretrieve(url, fname)
 '''
-
-from PyQt6.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QFrame
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtWidgets import QWidget, QApplication, QSlider, QVBoxLayout, QFrame
 from constructGUI import construct
 
 class Window(QWidget):
@@ -21,22 +21,10 @@ class Window(QWidget):
     def initUI(self):
         self.setWindowTitle("sample")
 
-        self.label_1 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_2 = construct(QLabel(), 'settings.yaml', 'label_2')
-        self.label_3 = construct(QLabel(), 'settings.yaml', 'label_3')
-        self.label_4 = construct(QLabel(), 'settings.yaml', 'label_4')
-        self.label_5 = construct(QLabel(), 'settings.yaml', 'label_5')
-        self.label_6 = construct(QLabel(), 'settings.yaml', 'label_6')
-        self.label_7 = construct(QLabel(), 'settings.yaml', 'label_7')
+        self.slider_1 = construct(QSlider(), 'settings.yaml', 'slider_1')
 
         layout = QVBoxLayout()
-        layout.addWidget(self.label_1)
-        layout.addWidget(self.label_2)
-        layout.addWidget(self.label_3)
-        layout.addWidget(self.label_4)
-        layout.addWidget(self.label_5)
-        layout.addWidget(self.label_6)
-        layout.addWidget(self.label_7)
+        layout.addWidget(self.slider_1)
 
         self.setLayout(layout)
 
