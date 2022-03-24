4ce3a3f30a6dd3dbb86cb3824e90646c01effe48
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-24 12:55:38 +0900

    update sample.py

--- sample.py
+++ sample.py
@@ -25,16 +25,36 @@ class Window(QWidget):
         self.label_2 = construct(QLabel(), 'settings.yaml', 'label_1')
         self.label_3 = construct(QLabel(), 'settings.yaml', 'label_1')
         self.label_4 = construct(QLabel(), 'settings.yaml', 'label_1')
+        self.label_5 = construct(QLabel(), 'settings.yaml', 'label_1')
+        self.label_6 = construct(QLabel(), 'settings.yaml', 'label_1')
+        self.label_7 = construct(QLabel(), 'settings.yaml', 'label_1')
 
+        self.label_2.setLineWidth(5)
         self.label_2.setFrameStyle(QFrame.Shape.Box.value)
-        self.label_3.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Raised.value)
-        self.label_4.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Sunken.value)
+
+        self.label_3.setLineWidth(5)
+        self.label_3.setFrameStyle(QFrame.Shape.Box.value | QFrame.Shadow.Raised.value)
+
+        self.label_4.setLineWidth(5)
+        self.label_4.setFrameStyle(QFrame.Shape.Box.value | QFrame.Shadow.Sunken.value)
+
+        self.label_5.setLineWidth(5)
+        self.label_5.setFrameStyle(QFrame.Shape.Panel.value)
+
+        self.label_6.setLineWidth(5)
+        self.label_6.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Raised.value)
+
+        self.label_7.setLineWidth(5)
+        self.label_7.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Sunken.value)
 
         layout = QVBoxLayout()
         layout.addWidget(self.label_1)
         layout.addWidget(self.label_2)
         layout.addWidget(self.label_3)
         layout.addWidget(self.label_4)
+        layout.addWidget(self.label_5)
+        layout.addWidget(self.label_6)
+        layout.addWidget(self.label_7)
 
         self.setLayout(layout)
 
