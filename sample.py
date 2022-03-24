e9a6b6c7e9c77ca35b211635261dac0fad000f7d
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-24 13:02:51 +0900

    add linewidth for qlabel

--- sample.py
+++ sample.py
@@ -29,22 +29,16 @@ class Window(QWidget):
         self.label_6 = construct(QLabel(), 'settings.yaml', 'label_1')
         self.label_7 = construct(QLabel(), 'settings.yaml', 'label_1')
 
-        self.label_2.setLineWidth(5)
         self.label_2.setFrameStyle(QFrame.Shape.Box.value)
 
-        self.label_3.setLineWidth(5)
         self.label_3.setFrameStyle(QFrame.Shape.Box.value | QFrame.Shadow.Raised.value)
 
-        self.label_4.setLineWidth(5)
         self.label_4.setFrameStyle(QFrame.Shape.Box.value | QFrame.Shadow.Sunken.value)
 
-        self.label_5.setLineWidth(5)
         self.label_5.setFrameStyle(QFrame.Shape.Panel.value)
 
-        self.label_6.setLineWidth(5)
         self.label_6.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Raised.value)
 
-        self.label_7.setLineWidth(5)
         self.label_7.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Sunken.value)
 
         layout = QVBoxLayout()
