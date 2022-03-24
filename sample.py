ebc202bb8fec9cb26aa045a4ae32bd6cb99c55b3
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-03-24 13:31:14 +0900

    add shape and shadow for qlabel

--- sample.py
+++ sample.py
@@ -22,24 +22,12 @@ class Window(QWidget):
         self.setWindowTitle("sample")
 
         self.label_1 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_2 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_3 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_4 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_5 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_6 = construct(QLabel(), 'settings.yaml', 'label_1')
-        self.label_7 = construct(QLabel(), 'settings.yaml', 'label_1')
-
-        self.label_2.setFrameStyle(QFrame.Shape.Box.value)
-
-        self.label_3.setFrameStyle(QFrame.Shape.Box.value | QFrame.Shadow.Raised.value)
-
-        self.label_4.setFrameStyle(QFrame.Shape.Box.value | QFrame.Shadow.Sunken.value)
-
-        self.label_5.setFrameStyle(QFrame.Shape.Panel.value)
-
-        self.label_6.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Raised.value)
-
-        self.label_7.setFrameStyle(QFrame.Shape.Panel.value | QFrame.Shadow.Sunken.value)
+        self.label_2 = construct(QLabel(), 'settings.yaml', 'label_2')
+        self.label_3 = construct(QLabel(), 'settings.yaml', 'label_3')
+        self.label_4 = construct(QLabel(), 'settings.yaml', 'label_4')
+        self.label_5 = construct(QLabel(), 'settings.yaml', 'label_5')
+        self.label_6 = construct(QLabel(), 'settings.yaml', 'label_6')
+        self.label_7 = construct(QLabel(), 'settings.yaml', 'label_7')
 
         layout = QVBoxLayout()
         layout.addWidget(self.label_1)
