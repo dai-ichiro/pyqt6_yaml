386d2744748bc7c80a8f1e6fec0f8e57b63f9cd0
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-04-13 19:23:42 +0900

    add color settings

--- sample.py
+++ sample.py
@@ -1,14 +1,3 @@
-'''
-import os
-from urllib.request import urlretrieve
-
-url = 'https://raw.githubusercontent.com/dai-ichiro/pyqt6_yaml/main/constructGUI.py'
-fname = os.path.basename(url)
-
-if not os.path.isfile(fname):
-    urlretrieve(url, fname)
-'''
-
 from PyQt6.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QFrame
 from constructGUI import construct
 
@@ -24,19 +13,11 @@ class Window(QWidget):
         self.label_1 = construct(QLabel(), 'settings.yaml', 'label_1')
         self.label_2 = construct(QLabel(), 'settings.yaml', 'label_2')
         self.label_3 = construct(QLabel(), 'settings.yaml', 'label_3')
-        self.label_4 = construct(QLabel(), 'settings.yaml', 'label_4')
-        self.label_5 = construct(QLabel(), 'settings.yaml', 'label_5')
-        self.label_6 = construct(QLabel(), 'settings.yaml', 'label_6')
-        self.label_7 = construct(QLabel(), 'settings.yaml', 'label_7')
 
         layout = QVBoxLayout()
         layout.addWidget(self.label_1)
         layout.addWidget(self.label_2)
         layout.addWidget(self.label_3)
-        layout.addWidget(self.label_4)
-        layout.addWidget(self.label_5)
-        layout.addWidget(self.label_6)
-        layout.addWidget(self.label_7)
 
         self.setLayout(layout)
 
