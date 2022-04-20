5d9b6da9f94e256a86c7993ecdeaba5d8c327b80
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-04-20 10:10:52 +0900

    rename label_sample.py and label_settings.yml

--- label_sample.py
+++ label_sample.py
@@ -1,6 +1,17 @@
+'''
+import os
+from urllib.request import urlretrieve
+
+url = 'https://raw.githubusercontent.com/dai-ichiro/pyqt6_yaml/main/qtyaml.py'
+fname = os.path.basename(url)
+
+if not os.path.isfile(fname):
+    urlretrieve(url, fname)
+'''
+
 from PyQt6.QtWidgets import QWidget, QApplication, QVBoxLayout
 
-from constructGUI2 import Label
+from qtyaml import Label
 
 class Window(QWidget):
 
@@ -10,8 +21,8 @@ class Window(QWidget):
         
     def initUI(self):
         
-        self.label_1 = Label('settings.yml', 'label_1')
-        self.label_2 = Label('settings.yml', 'label_2')
+        self.label_1 = Label('label_settings.yml', 'label_1')
+        self.label_2 = Label('label_settings.yml', 'label_2')
 
         layout = QVBoxLayout()
         layout.addWidget(self.label_1)
